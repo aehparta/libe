59df9a1d8321cfb29eb609e527aa0ec262206209
Antti Partanen <aehparta@iki.fi>
2019-01-30 23:15:20 +0200

    hd44780 avr testing in emulator

--- examples/config.h
+++ examples/config.h
@@ -4,33 +4,47 @@
 
 
 #ifdef TARGET_AVR
+/* spi */
 #define CFG_SPI_CONTEXT     NULL
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        0
 #define CFG_SPI_MOSI        0
 #define CFG_SPI_SCLK        0
+/* nrf24l01+ */
 #define CFG_NRF_SS          8
 #define CFG_NRF_CE          9
+/* hd44780 */
+#define CFG_HD44780_D4      8
+#define CFG_HD44780_D5      9
+#define CFG_HD44780_D6      10
+#define CFG_HD44780_D7      11
+#define CFG_HD44780_EN      14
+#define CFG_HD44780_RW      13
+#define CFG_HD44780_RS      12
 #endif
 
 
 #ifdef TARGET_PIC8
+/* spi */
 #define CFG_SPI_CONTEXT     NULL
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        0
 #define CFG_SPI_MOSI        0
 #define CFG_SPI_SCLK        0
+/* nrf24l01+ */
 #define CFG_NRF_SS          0
 #define CFG_NRF_CE          1
 #endif
 
 
 #ifdef TARGET_PIC32
+/* spi */
 #define CFG_SPI_CONTEXT     NULL
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        0
 #define CFG_SPI_MOSI        0
 #define CFG_SPI_SCLK        0
+/* nrf24l01+ */
 #define CFG_NRF_SS          0
 #define CFG_NRF_CE          1
 #endif
@@ -63,33 +77,39 @@
 
 
 #ifdef TARGET_MSP430
+/* spi */
 #define CFG_SPI_CONTEXT     NULL
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        0
 #define CFG_SPI_MOSI        0
 #define CFG_SPI_SCLK        0
+/* nrf24l01+ */
 #define CFG_NRF_SS          0
 #define CFG_NRF_CE          0
 #endif
 
 
 #ifdef TARGET_RPI
+/* spi */
 #define CFG_SPI_CONTEXT     NULL
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        0
 #define CFG_SPI_MOSI        0
 #define CFG_SPI_SCLK        0
+/* nrf24l01+ */
 #define CFG_NRF_SS          8
 #define CFG_NRF_CE          25
 #endif
 
 
 #ifdef TARGET_ESP32
+/* spi */
 #define CFG_SPI_CONTEXT     ((void *)HSPI_HOST)
 #define CFG_SPI_FREQUENCY   0
 #define CFG_SPI_MISO        GPIO_NUM_12
 #define CFG_SPI_MOSI        GPIO_NUM_13
 #define CFG_SPI_SCLK        GPIO_NUM_14
+/* nrf24l01+ */
 #define CFG_NRF_SS          GPIO_NUM_26
 #define CFG_NRF_CE          GPIO_NUM_27
 #endif
--- examples/hd44780/README.md
+++ examples/hd44780/README.md
@@ -10,3 +10,12 @@
     * BDBUS4: EN
     * BDBUS5: RW
     * BDBUS6: RS
+
+* avr on atmega328 in emulator not yet succesful totally, first line works
+    * PB0: D4
+    * PB1: D5
+    * PB2: D6
+    * PB3: D7
+    * PB4: RS
+    * PB5: RW
+    * PB6: EN
