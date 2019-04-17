cafc351ba5367cd80ea492359187d8e2847eee68
Antti Partanen <aehparta@iki.fi>
2019-04-17 17:47:01 +0300

    libe.h added

---
+++ libe/libe.h
@@ -0,0 +1,27 @@
+/*
+ * libe cross-platform library: Logging
+ *
+ * Authors:
+ *  Antti Partanen <aehparta@iki.fi>
+ */
+
+#ifndef _LIBE_LIBE_H_
+#define _LIBE_LIBE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <libe/os.h>
+#include <libe/log.h>
+#include <libe/wifi.h>
+#include <libe/i2c.h>
+#include <libe/nvm.h>
+#include <libe/aes.h>
+#include <libe/spi.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIBE_LIBE_H_ */
