5064ee2fcd6a3f58587007ac037314b9ad5e6206
Antti Partanen <aehparta@iki.fi>
2020-12-12 19:50:48 +0200

    ipecmd programming for pics

--- flash/avrdude.mk
+++ flash/avrdude.mk
@@ -1,13 +1,8 @@
 
 # programming support using avrdude
 
-# do not allow to include if target is wrong
-ifneq ($(TARGET),AVR)
-    $(error TARGET is not AVR)
-endif
-
 # avrdude base settings
-AVRDUDE = avrdude
+AVRDUDE ?= avrdude
 AVRDUDE_PROGRAMMER ?= avrisp2
 ifneq ($(programmer),)
     AVRDUDE_PROGRAMMER = $(programmer)
--- flash/program.mk
+++ flash/program.mk
@@ -2,5 +2,7 @@
 # include flash make info using target
 ifeq ($(TARGET),AVR)
 include $(LIBE_PATH)/flash/avrdude.mk
+else ifeq ($(TARGET),PIC8)
+include $(LIBE_PATH)/flash/ipecmd.mk
 else
 endif
