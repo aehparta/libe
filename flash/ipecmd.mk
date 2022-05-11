3a6226330501d8b71b6d4602469534f5a58cab70
Antti Partanen <aehparta@iki.fi>
2022-05-11 16:21:15 +0300

    sudo for ipecmd

--- flash/ipecmd.mk
+++ flash/ipecmd.mk
@@ -3,11 +3,17 @@
 
 # ipecmd base settings
 IPECMD ?= /opt/microchip/mplabx/v5.05/mplab_platform/mplab_ipe/ipecmd.sh
+ifeq ($(IPECMD_SUDO),1)
+    IPECMD_RUN ?= sudo $(IPECMD)
+else
+    IPECMD_RUN ?= $(IPECMD)
+endif
+
 IPECMD_PROGRAMMER ?= PK3
 ifneq ($(programmer),)
     IPECMD_PROGRAMMER = $(programmer)
 endif
 
 program: $(PROGRAM_BIN)
-	sudo $(IPECMD) -TP$(IPECMD_PROGRAMMER) -P$(MCU) -E -MP -OL -F$^$(TARGET_EXT).hex
+	$(IPECMD_RUN) -TP$(IPECMD_PROGRAMMER) -P$(MCU) -E -MP -OL -F$^$(TARGET_EXT).hex
 
