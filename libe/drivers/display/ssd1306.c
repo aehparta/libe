3951ea82d46d99c0ad55373bdb3af3a005b3f8f7
Antti Partanen <antti.partanen@abloy.com>
2025-04-25 12:59:01 +0300

    clipping

--- libe/drivers/display/ssd1306.c
+++ libe/drivers/display/ssd1306.c
@@ -5,36 +5,36 @@
 #include <libe/libe.h>
 
 
-const static uint8_t PROGMEM ssd1306_init_commands[] = {
-    SSD1306_DISPLAYOFF, // display off
-    SSD1306_MEMORYMODE,
-    SSD1306_HORIZONTAL_ADDRESSING_MODE, // Page Addressing mode
-    SSD1306_COMSCANDEC,                 // Scan from 127 to 0 (Reverse scan)
-    SSD1306_SETSTARTLINE | 0x00,        // First line to start scanning from
-    SSD1306_SETCONTRAST,
-    0x7f,                    // contrast value to 0x7F according to datasheet
-    SSD1306_SEGREMAP | 0x00, // Use reverse mapping. 0x00 - is normal mapping
-    SSD1306_NORMALDISPLAY,
-    SSD1306_SETMULTIPLEX,
-    63, // Reset to default MUX. See datasheet
-    SSD1306_SETDISPLAYOFFSET,
-    0x00, // no offset
-    SSD1306_SETDISPLAYCLOCKDIV,
-    0x80, // set to default ratio/osc frequency
-    SSD1306_SETPRECHARGE,
-    0x22, // switch precharge to 0x22 // 0xF1
-    SSD1306_SETCOMPINS,
-    0x12, // set divide ratio
-    SSD1306_SETVCOMDETECT,
-    0x20, // vcom deselect to 0x20 // 0x40
-    SSD1306_CHARGEPUMP,
-    0x14, // Enable charge pump
-    SSD1306_DISPLAYALLON_RESUME,
-    SSD1306_DISPLAYON
-};
-
 int8_t ssd1306_i2c_open(struct display *display, void *context, uint8_t address, int16_t w, int16_t h)
 {
+    uint8_t ssd1306_init_commands[] = {
+        SSD1306_DISPLAYOFF, // display off
+        SSD1306_MEMORYMODE,
+        SSD1306_HORIZONTAL_ADDRESSING_MODE, // Page Addressing mode
+        SSD1306_COMSCANDEC,                 // Scan from 127 to 0 (Reverse scan)
+        SSD1306_SETSTARTLINE | 0x00,        // First line to start scanning from
+        SSD1306_SETCONTRAST,
+        0x7f,                    // contrast value to 0x7F according to datasheet
+        SSD1306_SEGREMAP | 0x00, // Use reverse mapping. 0x00 - is normal mapping
+        SSD1306_NORMALDISPLAY,
+        SSD1306_SETMULTIPLEX,
+        63, // Reset to default MUX. See datasheet
+        SSD1306_SETDISPLAYOFFSET,
+        0x00, // no offset
+        SSD1306_SETDISPLAYCLOCKDIV,
+        0x80, // set to default ratio/osc frequency
+        SSD1306_SETPRECHARGE,
+        0x22, // switch precharge to 0x22 // 0xF1
+        SSD1306_SETCOMPINS,
+        0x12, // set divide ratio
+        SSD1306_SETVCOMDETECT,
+        0x20, // vcom deselect to 0x20 // 0x40
+        SSD1306_CHARGEPUMP,
+        0x14, // Enable charge pump
+        SSD1306_DISPLAYALLON_RESUME,
+        SSD1306_DISPLAYON
+    };
+
     address = address < 1 ? SSD1306_ADDR1 : address;
     IF_R(i2c_open(&display->i2c, context, address), -1);
 
@@ -58,16 +58,7 @@ int8_t ssd1306_i2c_open(struct display *display, void *context, uint8_t address,
     display->update = ssd1306_update;
 
     /* display init sequence */
-    IF_R(i2c_write(&display->i2c, (uint8_t *)ssd1306_init_commands, sizeof(ssd1306_init_commands)), -1);
-
-    // 	for (uint8_t i = 0; i < sizeof(ssd1306_init_commands); i++) {
-    // #ifdef TARGET_AVR
-    // 		uint8_t b = pgm_read_byte(&ssd1306_init_commands[i]);
-    // #else
-    // 		uint8_t b = ssd1306_init_commands[i];
-    // #endif
-    // 		IF_R(i2c_write(&display->i2c, &b, 1), -1);
-    // 	}
+    IF_R(i2c_write(&display->i2c, ssd1306_init_commands, sizeof(ssd1306_init_commands)), -1);
 
     return 0;
 }
